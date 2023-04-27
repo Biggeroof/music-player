0c949db5667a3684853f3442f1dc170d8b96345e
Shi Hai <shihai797@gmail.com>
2023-04-26 22:47:09 -0400

    playbar

--- music-player/src/App.tsx
+++ music-player/src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import List from "./components/List";
+import Playbar from "./components/Playbar";
 
 function App() {
-<<<<<<< HEAD
   let items = ["playlist 1", "playlist 2"];
   // const [isPlaying, setIsPlaying] = useState(False)
   const handleSelectItem = (item: string) => {
@@ -11,22 +11,16 @@ function App() {
   };
   return (
     <div>
-      <List
+      {/* <List
         items={items}
         heading="Playlists"
         onSelectItem={handleSelectItem}
         classNames="flex"
-      />
+      /> */}
+      {/* <Playbar /> */}
+      <Playbar />
     </div>
   );
-=======
-    useState(0);
-    return (
-        <>
-
-        </>
-    );
->>>>>>> 0b5152e96e1862b540b63ac654f7485e75c4138b
 }
 
 export default App;
--- music-player/src/components/Playbar.tsx
+++ music-player/src/components/Playbar.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Playbar = () => {
+  let [isPaused, setIsPaused] = useState(true);
+  let [shuffleOn, setShfuffleOn] = useState(false);
   return (
-    <div>Playbar</div>
-  )
-}
+    <div>
+      <button
+        type="button"
+        className="btn btn-default"
+        aria-label="Left Align"
+        onClick={() => {
+          setShfuffleOn(!shuffleOn);
+        }}
+      >
+        <span
+          className={
+            shuffleOn
+              ? "glyphicon glyphicon-random text-success"
+              : "glyphicon glyphicon-random"
+          }
+          aria-hidden="true"
+        ></span>
+      </button>
 
-export default Playbar
+      <button type="button" className="btn btn-default" aria-label="Left Align">
+        <span
+          className="glyphicon glyphicon-step-backward"
+          aria-hidden="true"
+        ></span>
+      </button>
+
+      {isPaused && (
+        <button
+          type="button"
+          className="btn btn-default"
+          aria-label="Left Align"
+          onClick={() => {
+            setIsPaused(!isPaused);
+          }}
+        >
+          <span className="glyphicon glyphicon-play" aria-hidden="true"></span>
+        </button>
+      )}
+
+      {!isPaused && (
+        <button
+          type="button"
+          className="btn btn-default"
+          aria-label="Left Align"
+          onClick={() => {
+            setIsPaused(!isPaused);
+          }}
+        >
+          <span className="glyphicon glyphicon-pause" aria-hidden="true"></span>
+        </button>
+      )}
+
+      <button type="button" className="btn btn-default" aria-label="Left Align">
+        <span
+          className="glyphicon glyphicon-step-forward"
+          aria-hidden="true"
+        ></span>
+      </button>
+    </div>
+  );
+};
+
+export default Playbar;
