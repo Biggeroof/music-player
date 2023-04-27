b010ce07b355a54fa820a7077d051f1eeb44ad16
Shi Hai <shihai797@gmail.com>
2023-04-27 16:22:40 -0400

    centered playbar vertically

--- music-player/src/components/Playbar.tsx
+++ music-player/src/components/Playbar.tsx
@@ -20,116 +20,127 @@ const Playbar = () => {
   }
 
   return (
-    <div className="bg-dark text-center text-lg-start fixed-bottom playbar d-flex justify-content-center">
-      <button
-        type="button"
-        className="btn btn-default"
-        aria-label="Left Align"
-        onClick={() => {
-          setShfuffleOn(!shuffleOn);
-        }}
-      >
-        <span
-          className={
-            shuffleOn
-              ? "glyphicon glyphicon-random text-success"
-              : "glyphicon glyphicon-random text-light"
-          }
-          aria-hidden="true"
-        ></span>
-      </button>
-
-      <button type="button" className="btn btn-default" aria-label="Left Align">
-        <span
-          className="glyphicon glyphicon-step-backward text-light"
-          aria-hidden="true"
-        ></span>
-      </button>
-
-      {isPaused && (
+    <>
+      <div className="bg-dark text-center text-lg-start fixed-bottom playbar d-flex">
+        <svg className="bi me-2" width="400" height="32"></svg>
         <button
           type="button"
           className="btn btn-default"
           aria-label="Left Align"
           onClick={() => {
-            setIsPaused(!isPaused);
+            setShfuffleOn(!shuffleOn);
           }}
         >
           <span
-            className="glyphicon glyphicon-play text-light"
+            className={
+              shuffleOn
+                ? "glyphicon glyphicon-random text-success"
+                : "glyphicon glyphicon-random text-light"
+            }
             aria-hidden="true"
           ></span>
         </button>
-      )}
 
-      {!isPaused && (
+        <button
+          type="button"
+          className="btn btn-default"
+          aria-label="Left Align"
+        >
+          <span
+            className="glyphicon glyphicon-step-backward text-light"
+            aria-hidden="true"
+          ></span>
+        </button>
+
+        {isPaused && (
+          <button
+            type="button"
+            className="btn btn-default"
+            aria-label="Left Align"
+            onClick={() => {
+              setIsPaused(!isPaused);
+            }}
+          >
+            <span
+              className="glyphicon glyphicon-play text-light"
+              aria-hidden="true"
+            ></span>
+          </button>
+        )}
+
+        {!isPaused && (
+          <button
+            type="button"
+            className="btn btn-default"
+            aria-label="Left Align"
+            onClick={() => {
+              setIsPaused(!isPaused);
+            }}
+          >
+            <span
+              className="glyphicon glyphicon-pause text-light"
+              aria-hidden="true"
+            ></span>
+          </button>
+        )}
+
+        <button
+          type="button"
+          className="btn btn-default"
+          aria-label="Left Align"
+        >
+          <span
+            className="glyphicon glyphicon-step-forward text-light"
+            aria-hidden="true"
+          ></span>
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-default text-light"
+          aria-label="Left Align"
+          onClick={() => {
+            setRepeatOn(!repeatOn);
+          }}
+        >
+          <span
+            className={
+              repeatOn
+                ? "glyphicon glyphicon-retweet text-success"
+                : "glyphicon glyphicon-retweet text-light"
+            }
+            aria-hidden="true"
+          ></span>
+        </button>
+        <svg className="bi me-2" width="200" height="32"></svg>
         <button
           type="button"
           className="btn btn-default"
           aria-label="Left Align"
           onClick={() => {
-            setIsPaused(!isPaused);
+            setMuted((m) => !m);
+
+            // make it so when you unmute it goes back to the volume before
+            setVolume((v) => (v !== 0 ? 0 : volumeBefore));
           }}
         >
-          <span
-            className="glyphicon glyphicon-pause text-light"
-            aria-hidden="true"
-          ></span>
+          <span className={volumeLabel()} aria-hidden="true"></span>
         </button>
-      )}
 
-      <button type="button" className="btn btn-default" aria-label="Left Align">
-        <span
-          className="glyphicon glyphicon-step-forward text-light"
-          aria-hidden="true"
-        ></span>
-      </button>
-
-      <button
-        type="button"
-        className="btn btn-default text-light"
-        aria-label="Left Align"
-        onClick={() => {
-          setRepeatOn(!repeatOn);
-        }}
-      >
-        <span
-          className={
-            repeatOn
-              ? "glyphicon glyphicon-retweet text-success"
-              : "glyphicon glyphicon-retweet text-light"
-          }
-          aria-hidden="true"
-        ></span>
-      </button>
-
-      <button
-        type="button"
-        className="btn btn-default"
-        aria-label="Left Align"
-        onClick={() => {
-          setMuted((m) => !m);
-
-          // make it so when you unmute it goes back to the volume before
-          setVolume((v) => (v !== 0 ? 0 : volumeBefore));
-        }}
-      >
-        <span className={volumeLabel()} aria-hidden="true"></span>
-      </button>
-
-      <section className="text-center" aria-label="Left Align">
-        <input
-          type="range"
-          min={0}
-          max={1}
-          step={0.02}
-          value={volume}
-          onChange={(event) => {
-            setVolume(event.target.valueAsNumber);
-          }}
-        />
-      </section>
-    </div>
+        <section className="text-center centerdiv" aria-label="Left Align">
+          <input
+            type="range"
+            min={0}
+            max={1}
+            step={0.02}
+            value={volume}
+            onChange={(event) => {
+              setVolume(event.target.valueAsNumber);
+            }}
+          />
+        </section>
+      </div>
+    </>
   );
 };
 
