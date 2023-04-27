0b5152e96e1862b540b63ac654f7485e75c4138b
Edmond <liedmond0527@gmail.com>
2023-04-26 21:59:49 -0400

    modfied stuff in components and app

--- music-player/src/App.tsx
+++ music-player/src/App.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+    useState(0);
+    return (
+        <>
+
+        </>
+    );
+}
 
 export default App;
