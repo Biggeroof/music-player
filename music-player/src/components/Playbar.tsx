48a901d61f8a66246db48513ded17ba4e5c45951
Shi Hai <shihai797@gmail.com>
2023-04-26 23:35:08 -0400

    centered playbar

--- music-player/src/components/Playbar.tsx
+++ music-player/src/components/Playbar.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import '../css/playbar.css'
+import "../css/playbar.css";
 
 const Playbar = () => {
   let [isPaused, setIsPaused] = useState(true);
   let [shuffleOn, setShfuffleOn] = useState(false);
   return (
-    <div className="bg-dark text-center text-lg-start fixed-bottom playbar">
+    <div className="bg-dark text-center text-lg-start fixed-bottom playbar d-flex justify-content-center">
       <button
         type="button"
         className="btn btn-default"
