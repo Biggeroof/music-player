7f420e9ca5763ee559bc36449ef2c40a71d9d55a
Shi Hai <shihai797@gmail.com>
2023-04-26 22:56:20 -0400

    a

--- music-player/src/App.tsx
+++ music-player/src/App.tsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import List from "./components/List";
-<<<<<<< HEAD
-import Playbar from "./components/Playbar";
-=======
 import SideBar from "./components/Sidebar";
 import Song from "./components/Song";
 import Playbar from "./components/Playbar";
 import Hero from "./components/Hero";
->>>>>>> 6605c3ccaea10953dad894819c5faf71b1795001
 
 function App() {
   let items = ["playlist 1", "playlist 2"];
@@ -19,26 +15,15 @@ function App() {
 
   return (
     <div>
-<<<<<<< HEAD
-      {/* <List
+      <Hero />
+      <SideBar />
+      <List
         items={items}
         heading="Playlists"
         onSelectItem={handleSelectItem}
         classNames="flex"
-      /> */}
-      {/* <Playbar /> */}
+      />
       <Playbar />
-=======
-        <Hero />
-        <SideBar />
-        <List
-            items={items}
-            heading="Playlists"
-            onSelectItem={handleSelectItem}
-            classNames="flex"
-        />
-        <Playbar />
->>>>>>> 6605c3ccaea10953dad894819c5faf71b1795001
     </div>
   );
 }
--- music-player/src/components/Sidebar.tsx
+++ music-player/src/components/Sidebar.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import "../css/sidebar.css";
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
-  )
-}
+    <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark sideBar">
+      <a
+        href="/"
+        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+      >
+        <svg className="bi me-2" width="40" height="32"></svg>
+        <span className="fs-4">Sidebar</span>
+      </a>
+      <ul className="nav nav-pills flex-column mb-auto">
+        <li className="nav-item">
+          <a href="#" className="nav-link active" aria-current="page">
+            <svg className="bi me-2" width="16" height="16"></svg>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-link text-white">
+            <svg className="bi me-2" width="16" height="16"></svg>
+            Dashboard
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-link text-white">
+            <svg className="bi me-2" width="16" height="16"></svg>
+            Orders
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-link text-white">
+            <svg className="bi me-2" width="16" height="16"></svg>
+            Products
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-link text-white">
+            <svg className="bi me-2" width="16" height="16"></svg>
+            Customers
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default SideBar
+export default SideBar;
