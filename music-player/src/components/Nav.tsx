9ec095cc87a5a95a71a6a0bd923ff79041f294d0
Shi Hai <shihai797@gmail.com>
2023-04-28 13:44:57 -0400

    a

--- music-player/src/components/Nav.tsx
+++ music-player/src/components/Nav.tsx
@@ -1,14 +1,21 @@
-import '../css/Nav.css'
+import "../css/Nav.css";
 
 const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark justify-content-center">
-        <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+      <form className="form-inline my-2 my-lg-0">
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
