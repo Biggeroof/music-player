6ac4d4589fd0820eb9cc78a1210096c84a1b9168
Edmond <liedmond0527@gmail.com>
2023-04-30 18:06:29 -0400

    added hero

---
+++ music-player/src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Home from "../pages/Home";
+import PlaylistTab from "../pages/PlaylistTab";
+import "../css/hero.css";
+
+const Hero = ({ currPlaylist: Playlist }) => {
+  const [isHome, setIsHome] = useState(false);
+
+  return(
+    <>
+      <button
+        type="button"
+        className="backbtn btn"
+        onClick={() => {
+          setIsHome(!isHome);
+        }}
+      >
+        <span
+          className="glyphicon glyphicon-arrow-left text-light"
+          aria-hidden="true"
+        ></span>
+      </button>
+      <PlaylistTab currPlaylist={currPlaylist} />
+      {/* {isHome && <Home />}
+      {!isHome && <PlaylistTab />} */}
+    </>
+  );
+};
+
+export default Hero;
