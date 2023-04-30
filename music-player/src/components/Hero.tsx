7b90fa97d94b3f69cde9fe6f2d9e640d8508e9ca
Shi Hai <shihai797@gmail.com>
2023-04-30 16:51:34 -0400

    add back btn

--- music-player/src/components/Hero.tsx
+++ music-player/src/components/Hero.tsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import Home from "../pages/Home";
 import PlaylistTab from "../pages/PlaylistTab";
+import "../css/hero.css";
 
 const Hero = () => {
   const [isHome, setIsHome] = useState(true);
 
   return (
     <>
+      <button
+        type="button"
+        className="backbtn btn"
+        onClick={() => {
+          setIsHome(!isHome);
+        }}
+      >
+        <span
+          className="glyphicon glyphicon-arrow-left text-light"
+          aria-hidden="true"
+        ></span>
+      </button>
       {isHome && <Home />}
       {!isHome && <PlaylistTab />}
     </>
-   
   );
 };
 
