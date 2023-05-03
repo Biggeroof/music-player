10a04dc71d4916481c37fcdacb7b1db9efb38943
Shi Hai <shihai797@gmail.com>
2023-05-03 16:37:49 -0400

    my head hurts

--- music-player/src/utils.jsx
+++ music-player/src/utils.jsx
@@ -3,3 +3,33 @@ export function youtube_parser(url){
     var match = url.match(regExp);
     return (match&&match[7].length==11)? match[7] : false;
 }
+
+
+export function importAllo(){
+    let playlists = []
+    fetch('src/playlists/master.json')
+    .then((response) => response.json())
+    .then((json) => {
+        let playlistPaths = json['title']
+        playlistPaths.map((item) => {
+
+            fetch(item)
+            .then((response) => {
+                if (response.status !== 200)  {
+                    console.log('error')
+                    return;
+                }
+                response.json().then((json) => {
+                    playlists.push(json)
+                })
+            })
+
+            .catch((err) => {
+                console.log('error')
+                console.log('b')
+                return;
+            })
+            console.log('d')
+        })
+    });
+    return playlists}
